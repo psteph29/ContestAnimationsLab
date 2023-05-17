c7ce1e850684f29ee2981027ff3aa0b395c32ff2
Paige Stephenson <paigestephenson29@gmail.com>
2023-05-17 09:32:57 -0600

    Initial Commit

---
+++ Contest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contest
+//
+//  Created by Paige Stephenson on 5/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
