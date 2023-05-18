d099eac85b541d397423985e38ec00036104874a
Paige Stephenson <paigestephenson29@gmail.com>
2023-05-17 18:18:02 -0600

    Add animation file

--- Contest/ViewController.swift
+++ Contest/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        if emailTextField.text == "" {
+           shakeTextBox()
+        } else {
+            performSegue(withIdentifier: "entrySuccess", sender: nil)
+        }
+        
+    }
+    
+    func shakeTextBox() {
+        UIView.animate(withDuration: 0.04, animations: {
+            self.emailTextField.transform = CGAffineTransform(scaleX: 10, y: 1.3)
+        }) { _ in
+            UIView.animate(withDuration: 0.07, animations: {
+                self.emailTextField.transform = CGAffineTransform(translationX: -5, y: 1.3)
+            }) {
+                _ in
+                UIView.animate(withDuration: 0.07, animations: {
+                    self.emailTextField.transform = CGAffineTransform.identity
+                })
+            }
+        }
+    }
+    
 }
-
